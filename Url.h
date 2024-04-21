cbede9e9ace883671750bb20dae7a753edc7f182
Filipe Magalhães <filipepio2005@hotmail.com>
2024-04-21 14:02:01 -0300

    Update Url.h

--- Url.h
+++ Url.h
@@ -9,15 +9,21 @@
 
 using namespace std;
 
-class Url{
+#ifndef URL_H
+#define URL_H
+
+
+class Url{ // Declaração/Interface para programa
   private: 
-    stack<string> previousPages;
-    string currentPage;
-    stack<string> posteriorPages;
+    stack<string> previousPages; // Pilha para armazenar as páginas anteriores
+    string currentPage; // String para armazenar a página atual
+    stack<string> posteriorPages; // Pilha para armazenar as páginas posteriores
   
   public: 
-    void newPage(const string& newURL);
-    void goBack();
-    void goFoward();
-    void getPage();
+    void newPage(const string& newURL); // Método para adicionar uma nova página
+    void goBack(); // Método para voltar para a página anterior
+    void goFoward(); // Método para avançar para a página seguinte
+    void getPage(); // Método para exibir a página atual
 };
+
+#endif // URL_H
